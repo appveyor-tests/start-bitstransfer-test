7d2d8a3a7bf63a546362064c12deceb23f35aba1
Feodor Fitsner <feodor@appveyor.com>
2017-07-20 15:16:38 -0700

    Run via PowerShell.exe

---
+++ test-bitstransfer.ps1
@@ -0,0 +1,4 @@
+$url = 'https://storage.googleapis.com/dart-archive/channels/dev/raw/1.25.0-dev.4.0/sdk/dartsdk-windows-x64-release.zip'
+$fileName = "$env:temp\dartsdk-windows-x64-release.zip"
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $fileName
