3e90a71dccd915435686e51d50d2df184800b019
Feodor Fitsner <feodor@appveyor.com>
2017-07-20 15:18:04 -0700

    Fail on error

--- test-bitstransfer.ps1
+++ test-bitstransfer.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = "Stop"
 $url = 'https://storage.googleapis.com/dart-archive/channels/dev/raw/1.25.0-dev.4.0/sdk/dartsdk-windows-x64-release.zip'
 $fileName = "$env:temp\dartsdk-windows-x64-release.zip"
 Import-Module BitsTransfer
